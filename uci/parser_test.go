5bf364fc9735135edc9ca5db81f6e7ab9bf97478
Matthias Derer <matthias.derer@esono.de>
2020-06-16 22:17:04 +0200

    add basis for position tests

--- uci/parser_test.go
+++ uci/parser_test.go
@@ -6,7 +6,11 @@ import (
 
 func TestSetOption(t *testing.T) {
 
-	expectations := []struct{source string; name string; value string}{
+	expectations := []struct {
+		source string
+		name   string
+		value  string
+	}{
 		{
 			source: "setoption name foo value bar\n",
 			name:   "foo",
@@ -48,6 +52,42 @@ func TestSetOption(t *testing.T) {
 			t.Error("expected Value to be", expectation.value, "but got", stmt.SetOption.Value)
 		}
 	}
+}
 
+func TestPosition(t *testing.T) {
+	expectations := []struct {
+		source     string
+		kind GoKind
+		fenString  string
+		isFen      bool
+		isStartPos bool
+		moves      []string
+	}{
+		{
+			source:     "go searchmoves e1f1\n",
+			kind: 		Go_searchMovesKind,
+			fenString:  "0000",
+			isFen:      true,
+			isStartPos: false,
+			moves:      []string{"e1f1"},
+		},
+	}
 
+	for _, expectation := range expectations {
+		statements, err := Parse(expectation.source)
+		if err != nil {
+			t.Fatal("error parsing test source", err)
+		}
+		if len(statements) != 1 {
+			t.Errorf("expected 1 statement, but got %d", len(statements))
+		}
+		stmnt := statements[0]
+		if stmnt.Kind != GoStatementKind {
+			t.Fatal("Expected go kind")
+		}
+		if stmnt.Go.Kind != expectation.kind {
+			t.Fatal("Expected kind", expectation.kind, "but got", stmnt.Go.Kind)
+		}
+
+	}
 }
